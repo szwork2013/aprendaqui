e8a64c8fdfa6507bb5ae71439c4fc409831f5162
sergioaugrod <sergioaugrod@gmail.com>
2015-07-02 13:32:04 -0300

    Fix mongoose version, oauth secret samples

--- server/config/oauth.js
+++ server/config/oauth.js
@@ -1,13 +1,13 @@
 module.exports = {
     /* Configuracões OATUH FACEBOOK */
     'facebook': {
-        'api_key': '',
-        'api_secret': '',
-        'callback_url': ''
+        'api_key': 'INSERT_YOUR_API_KEY',
+        'api_secret': 'INSERT_YOUR_API_SECRET',
+        'callback_url': 'INSERT_YOUR_CALLBACK_URL'
     },
     'google': {
-        'clientID': '',
-        'clientSecret': '',
-        'callbackURL': ''
+        'clientID': 'INSERT_YOUR_CLIENT_ID',
+        'clientSecret': 'INSERT_YOUR_CLIENT_SECRET',
+        'callbackURL': 'INSERT_YOUR_CALLBACK_URL'
     }
 };
